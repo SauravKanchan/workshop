e45ddae403b8687c4c9c73f9a14f1b833b1627a6
SauravKanchan <sauravnk30@gmail.com>
2019-09-14 16:10:50 +0530

    update dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,9 +1,17 @@
-students = dict()
+meaning = {
+    "asdf": "It's a string",
+    "asf":"asfd"
+}
 
-students["one"] = "new value"
-
-students["two"] = "another new value"
-students["three"] = "another new value"
-
-del students["two"]
-
+meaning["1"] = "It is a number"
+meaning["True"] = "It is a boolean"
+try:
+    print(meaning[input("Enter your word")])
+except KeyError:
+    print("Word not in dictionary")
+'''
+Task is to make a program 
+which takes word/key as input from user
+and print its meaning/value 
+using dictionary
+'''
