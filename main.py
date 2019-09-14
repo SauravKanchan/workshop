f6e48df395e79bb55baf8fb76f5c247fd64acd9c
SauravKanchan <sauravnk30@gmail.com>
2019-09-14 11:46:40 +0530

    add while loop

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-for i in range(0,5):
-    print("Hello", i)
-    print(2*i)
+number = 4
+
+for i in range(1,13):
+    print(number, "X",i,"=", i*number )
 
 print("End of loop")
+
+count = 0
+while count<=12:
+    print(number, "X",count,"=", count*number )
+    count = count +1
+
+print("End of while loop")
