a51bef91c24960e056e10acff19e28c552d540e5
SauravKanchan <sauravnk30@gmail.com>
2019-09-14 15:39:41 +0530

    add list

---
+++ List.py
@@ -0,0 +1,9 @@
+students = ["jhon","doe","asdf"]
+print(students[0])
+print(len(students))
+print(students[0:2])
+print(list("saurav"))
+string = "data1 data2 data3"
+list_string = string.split(" ")
+for i in list_string:
+    print(i)
--- calculator.py
+++ calculator.py
@@ -5,6 +5,14 @@ choice = -1
 3 is division
 0
 '''
+'''
+Task:
+Ensure that b is not 0
+if user enters b as 0
+then tell him that it is not possible.
+
+'''
+
 while choice != 0:
     a=int(input("Enter your first number"))
     b=int(input("Enter your second number"))
@@ -14,7 +22,10 @@ while choice != 0:
     elif choice==2:
         print(a-b)
     elif choice == 3:
-        print(a/b)
+        try:
+            print(a/b)
+        except ZeroDivisionError:
+            print("b cannot be 0")
     elif choice == 0:
         break
     else:
