ebb7ed0b7a1d1743c1ec3da094fca38a77d61c77
SauravKanchan <sauravnk30@gmail.com>
2019-09-14 12:02:50 +0530

    add userinput

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-number = 4
+data = input("Enter your data")
+number = int(data) 
 
+# this for loop which starts from 0 and ends at 12.k 
 for i in range(1,13):
     print(number, "X",i,"=", i*number )
 
 print("End of loop")
-
+'''
 count = 0
 while count<=12:
     print(number, "X",count,"=", count*number )
     count = count +1
 
 print("End of while loop")
+'''
+print("End of program")
---
+++ user_input.py
@@ -0,0 +1,5 @@
+print("Enter your data")
+data = input() # 3 will be in string format
+integer_data = int(data) # Convert 3 into an integer 
+for i in range(5):
+    print(integer_data*i)
