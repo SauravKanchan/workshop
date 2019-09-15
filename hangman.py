903a91612b97b02c2657129bbfd82c4d1af56fe6
SauravKanchan <sauravnk30@gmail.com>
2019-09-15 14:52:55 +0530

    update hangman

--- hangman.py
+++ hangman.py
@@ -1,9 +1,20 @@
-word = "secrate"
+word = "asdf"
 count = 10
+guesses = []
+def output(guess_array,word="secrate"):
+    for w in word:
+        if w in guess_array:
+            print(w, end=" ")
+        else:
+            print("_", end=" ")
+    print()
+
 for i in range(count):
     guess = input("Type your guess")
-    if word == guess:
-        print("Matched")
-        break
-    else:
-        print("Not matched",count-i-1,"tries remaining")
+    guesses.append(guess)
+    output(guesses, word)
+    # if word == guess:
+    #     print("Matched")
+    #     break
+    # else:
+    #     print("Not matched",count-i-1,"tries remaining")
